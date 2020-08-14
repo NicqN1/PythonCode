e4541c236f50211c178d307173c154e8b597d1fd
NicqN1 <69662755+NicqN1@users.noreply.github.com>
2020-08-14 12:36:54 +0800

    Create examgenerator.py

---
+++ examgenerator.py
@@ -0,0 +1,5 @@
+import random
+glossary = ["Analyse", "Calculate", "Discuss", "Identify", "Predict"]
+meme = ["Abstraction", "Decomposition", "Stepwise Refinement", "Structered English"]
+print("1. Explain what",random.choice(glossary),"means")
+print("2. Define:",random.choice(meme))
