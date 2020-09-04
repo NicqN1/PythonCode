3cfdeae8276cc7dc2f79bc6ec8d0f3441ae284a5
NicqN1 <69662755+NicqN1@users.noreply.github.com>
2020-09-04 23:05:15 +0800

    Add files via upload

--- A229.py
+++ A229.py
@@ -1,4 +1,5 @@
 import random
+import uuid
 ipv6hex = ("a","b","c","d","e","f")
 ipv6num = (1,2,3,4,5,6,7,8,9)
 ipv41 = []
@@ -8,15 +9,11 @@ ipv63 = []
 ipv64 = []
 for x in range(199):
     ipv41.append(x+1)
-for i in range(2):
-    ipv61.append(random.choice(ipv6hex))
-    ipv61.append(random.choice(ipv6num))
-    ipv62.append(random.choice(ipv6hex))
-    ipv62.append(random.choice(ipv6num))
-    ipv63.append(random.choice(ipv6hex))
-    ipv63.append(random.choice(ipv6num))
-    ipv64.append(random.choice(ipv6hex))
-    ipv64.append(random.choice(ipv6num))
-
-print("ipv4 generated:",random.choice(ipv41),":",random.choice(ipv41),":",random.choice(ipv41),":",random.choice(ipv41))
-print("ipv6 generated:",ipv61,":",ipv62,":",ipv63,":",ipv64)
+def my_random_string(string_length=10):
+    random = str(uuid.uuid4())
+    random = random.upper()
+    random = random.replace("-","")
+    return random[0:string_length]
+for i in range(0,99):
+    print("ipv6 generated:",my_random_string(4),":",my_random_string(4),":",my_random_string(4),":",my_random_string(4))
+    print("ipv4 generated:",random.choice(ipv41),":",random.choice(ipv41),":",random.choice(ipv41),":",random.choice(ipv41))
